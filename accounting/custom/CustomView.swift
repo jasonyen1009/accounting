4f22a45d46273664a95a4e03f174e92a166bf749
Jason <jasonyen1009@gmail.com>
2023-09-22 21:59:00 +0800

    v 8.0 將整個專案分類成 MVC 架構

---
+++ accounting/custom/CustomView.swift
@@ -0,0 +1,26 @@
+//
+//  CustomView.swift
+//  accounting
+//
+//  Created by Hong Cheng Yen on 2022/7/17.
+//
+
+import UIKit
+
+// 好像沒有使用到
+//class CustomView: UIView {
+//
+//    let segmentedControl: UISegmentedControl = {
+//            let items = ["Now","In 15 mins", "In 1 hour"]
+//            let sc = UISegmentedControl(items: items)
+//            sc.selectedSegmentIndex = 0
+//            return sc
+//      }()
+//
+//    let fetchingLabel: UILabel = {
+//           let label = UILabel(frame: .zero)
+//           label.text = "Fetching..."
+//           return label
+//      }()
+//
+//}
